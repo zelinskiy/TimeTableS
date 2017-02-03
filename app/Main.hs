323006af4b0d114e84eb62a57c6e35f3246ef096
zelinskiy <zelinskiy1917@gmail.com>
2017-02-03 21:14:45 +0200

    Timetable types work

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,8 @@
 module Main where
 
 import Data.Aeson
-import Data.Text
+import Data.Aeson.Types
+import Data.Text hiding (drop, reverse)
 import Control.Applicative
 import Control.Monad
 import qualified Data.ByteString.Lazy as B
@@ -41,18 +42,82 @@ data GroupsRoot =
 instance FromJSON GroupsRoot
 instance ToJSON GroupsRoot
 
-allFacsUrl = "http://cist.nure.ua/ias/app/tt/get_faculties"
-csFacUrl = "http://cist.nure.ua/ias/app/tt/get_groups?faculty_id=95"
+data Teacher = Teacher { teacher_name :: !Text } deriving (Show,Generic)
+
+instance FromJSON Teacher
+instance ToJSON Teacher
+
+data Auditory =
+  Auditory { auditory_name      :: !Text
+            , auditory_address  :: !Text
+            } deriving (Show,Generic)
+
+instance FromJSON Auditory
+instance ToJSON Auditory
+
+data Lesson =
+  Lesson {  _subject    :: !Text
+          , _type       :: Int
+          , _time_start :: String
+          , _time_end   :: String
+          , _date_start :: Maybe String
+          , _date_end   :: Maybe String
+          , _dates      :: Maybe [String]
+          , _teachers   :: [Teacher]
+          , _auditories :: [Auditory]
+          } deriving (Show,Generic)
+
+instance ToJSON Lesson where
+  toJSON = genericToJSON defaultOptions {
+             fieldLabelModifier = drop 1 }
+
+instance FromJSON Lesson where
+  parseJSON = genericParseJSON defaultOptions {
+                fieldLabelModifier = drop 1 }
+
+data Day =
+  Day { weekday  :: Int
+      , lessons  :: [Lesson]
+      } deriving (Show,Generic)
+
+instance FromJSON Day
+instance ToJSON Day
+
+data TimeTable =
+  TimeTable { group_nameTT  :: !Text
+            , daysTT        :: [Day]
+            } deriving (Show,Generic)
+
+dropLast n = reverse . (drop n) . reverse
+
+instance ToJSON TimeTable where
+  toJSON = genericToJSON defaultOptions {
+             fieldLabelModifier = dropLast 2 }
+
+instance FromJSON TimeTable where
+  parseJSON = genericParseJSON defaultOptions {
+                fieldLabelModifier = dropLast 2 }
+
+
+
+
 
 getJSON :: String -> IO B.ByteString
 getJSON url = simpleHttp url
 
+allFacsUrl = "http://cist.nure.ua/ias/app/tt/get_faculties"
+csFacUrl = "http://cist.nure.ua/ias/app/tt/get_groups?faculty_id=95"
+timetableUrl = "http://cist.nure.ua/ias/app/tt/get_schedule?group_id=5259356"
+
 getFacultiesRoot :: IO (Either String FacultiesRoot)
 getFacultiesRoot = eitherDecode <$> (getJSON allFacsUrl)
 
+getTimetableRoot :: IO (Either String TimeTable)
+getTimetableRoot = eitherDecode <$> (getJSON timetableUrl)
+
 main :: IO ()
 main = do
- d <- getFacultiesRoot
+ d <- getTimetableRoot
  case d of
   Left err -> putStrLn err
   Right ps -> print ps
