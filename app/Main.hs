8038afaaf9512d528e903a4f3125ebb30a464b12
zelinskiy <zelinskiy1917@gmail.com>
2017-02-03 20:16:10 +0200

    Faculties list Types

--- app/Main.hs
+++ app/Main.hs
@@ -11,25 +11,31 @@ import GHC.Generics
 
 import Lib
 
-data Person =
-  Person { firstName  :: !Text
-         , lastName   :: !Text
-         , age        :: Int
-         , likesPizza :: Bool
-           } deriving (Show,Generic)
+data Faculty =
+  Faculty { faculty_name  :: !Text
+            , faculty_id  :: Int
+            , date_start  :: !Text
+            , date_end    :: !Text
+            } deriving (Show,Generic)
 
-instance FromJSON Person
-instance ToJSON Person
+instance FromJSON Faculty
+instance ToJSON Faculty
+
+data FacultiesRoot =
+  FacultiesRoot { faculties  :: [Faculty] } deriving (Show,Generic)
+
+instance FromJSON FacultiesRoot
+instance ToJSON FacultiesRoot
 
 jsonURL :: String
-jsonURL = "http://daniel-diaz.github.io/misc/pizza.json"
+jsonURL = "http://cist.nure.ua/ias/app/tt/get_faculties"
 
 getJSON :: IO B.ByteString
 getJSON = simpleHttp jsonURL
 
 main :: IO ()
 main = do
- d <- (eitherDecode <$> getJSON) :: IO (Either String [Person])
+ d <- (eitherDecode <$> getJSON) :: IO (Either String FacultiesRoot)
  case d of
   Left err -> putStrLn err
   Right ps -> print ps
