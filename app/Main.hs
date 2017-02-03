5c1a4e555e8b300fbd17d0b7ccfcab4b9b399d25
zelinskiy <zelinskiy1917@gmail.com>
2017-02-03 20:09:33 +0200

    example code

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,35 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 module Main where
+
+import Data.Aeson
+import Data.Text
+import Control.Applicative
+import Control.Monad
+import qualified Data.ByteString.Lazy as B
+import Network.HTTP.Conduit (simpleHttp)
+import GHC.Generics
 
 import Lib
 
+data Person =
+  Person { firstName  :: !Text
+         , lastName   :: !Text
+         , age        :: Int
+         , likesPizza :: Bool
+           } deriving (Show,Generic)
+
+instance FromJSON Person
+instance ToJSON Person
+
+jsonURL :: String
+jsonURL = "http://daniel-diaz.github.io/misc/pizza.json"
+
+getJSON :: IO B.ByteString
+getJSON = simpleHttp jsonURL
+
 main :: IO ()
-main = someFunc
+main = do
+ d <- (eitherDecode <$> getJSON) :: IO (Either String [Person])
+ case d of
+  Left err -> putStrLn err
+  Right ps -> print ps
